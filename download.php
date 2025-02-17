9b2cc905d06a8a24ff52db5357d729fd536de32d
Jordi Boggiano <j.boggiano@seld.be>
2025-02-17 11:43:31 +0100

    Update to make use of packagist metadata v2

--- download.php
+++ download.php
@@ -1,12 +1,14 @@
 <?php error_reporting(E_ALL);
 
-function getTopPackages($min, $max) {
-    $perPage = 15;
+require 'vendor/autoload.php';
+
+function getTopPackages(int $min, int $max) {
+    $perPage = max(1, min($max - $min, 100));
     $page = intdiv($min, $perPage);
     $id = $page * $perPage;
     while (true) {
         $page++;
-        $url = 'https://packagist.org/explore/popular.json?page=' . $page;
+        $url = 'https://packagist.org/explore/popular.json?page=' . $page.'&per_page='.$perPage;
         $json = json_decode(file_get_contents($url), true);
         foreach ($json['packages'] as $package) {
             yield $id => $package['name'];
@@ -31,21 +33,38 @@ $maxPackage = $argv[2];
 foreach (getTopPackages($minPackage, $maxPackage) as $i => $packageName) {
     echo "[$i] $packageName\n";
     $packageName = strtolower($packageName);
-    $url = 'https://repo.packagist.org/p/' . $packageName . '.json';
+    $url = 'https://repo.packagist.org/p2/' . $packageName . '~dev.json';
     $json = json_decode(file_get_contents($url), true);
-    $versions = $json['packages'][$packageName];
-    if (isset($versions['dev-master'])) {
-        $version = 'dev-master';
-    } else {
-        // Pick last version.
-        $keys = array_keys($versions);
-        $version = $keys[count($keys) - 1];
+    $versions = \Composer\MetadataMinifier\MetadataMinifier::expand($json['packages'][$packageName]);
+    unset($package);
+    // select default branch if available
+    foreach ($versions as $version) {
+        if (true === ($version['default-branch'] ?? false)) {
+            $package = $version;
+            break;
+        }
+    }
+    if (!isset($package)) {
+        // or any branch
+        if (count($versions) > 0) {
+            $package = reset($versions);
+        } else {
+            // or any version
+            $url = 'https://repo.packagist.org/p2/' . $packageName . '~dev.json';
+            $json = json_decode(file_get_contents($url), true);
+            $versions = \Composer\MetadataMinifier\MetadataMinifier::expand($json['packages'][$packageName]);
+            if (count($versions) > 0) {
+                $package = reset($versions);
+            } else {
+                echo "Skipping due to no version found\n";
+                continue;
+            }
+        }
     }
 
     // Thanks to people excluding tests from dist packages,
     // we're forced to clone source repos here.
-    $package = $versions[$version];
-    if ($package['source'] === null) {
+    if (($package['source'] ?? null) === null) {
         echo "Skipping due to missing source\n";
         continue;
     }
@@ -57,7 +76,7 @@ foreach (getTopPackages($minPackage, $maxPackage) as $i => $packageName) {
     $git = $package['source']['url'];
     $repo = __DIR__ . '/repos/' . $packageName;
     if (!is_dir($repo)) {
-        echo "Cloning $packageName @ $version from $git...\n";
+        echo "Cloning $packageName @ {$package['version']} from $git...\n";
         exec("git clone $git $repo", $execOutput, $execRetval);
         if ($execRetval !== 0) {
             echo "git clone failed: $execOutput\n";
